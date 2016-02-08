a2d724b702d81eddc291ab299f823d3009aad8d5
David Wallin <dedoubleyou1@gmail.com>
2016-02-08 13:26:50 -0500

    New Arrow System

--- Assets/scripts/dancing.cs
+++ Assets/scripts/dancing.cs
@@ -4,25 +4,29 @@ using System.Collections;
 public class dancing : MonoBehaviour {
 
 	Animator animator;
+	Animator arrowAnimator;
 	AudioSource myAudio; 
-	private arrowManager arrowManage;
+	//private arrowManager arrowManage;
 	private GameManager gameManager;
-	public GameObject arrow;
+	//public GameObject arrow;
 	public ParticleSystem heartParticles;
+	public GameObject arrowNew;
+	public float timeBeforeChangeDirection;
 
 
 	float  changeDirectionTimer = 0;
-	float  timeBeforeChangeDirection = 2;
+	//float  timeBeforeChangeDirection = 2;
 	string currentDirection = "left";
 	bool   moveMadeForThisTurn = false;
 	int    playerDancingHealth = 10;
 
 	void Start () {
-
+		Debug.Log (arrowNew);
 		myAudio = GetComponent<AudioSource> (); 
 		gameManager = Camera.main.GetComponent<GameManager> ();
-		arrowManage = arrow.GetComponent<arrowManager> ();
+	//	arrowManage = arrow.GetComponent<arrowManager> ();
 		animator = GetComponent<Animator> ();
+		arrowAnimator = arrowNew.GetComponent<Animator>();
 		ChangeDirection ();
 	}
 
@@ -32,7 +36,7 @@ public class dancing : MonoBehaviour {
 		// Check for game over
 		if (playerDancingHealth <= 0) {
 			animator.SetInteger ("state", 0);
-			arrowManage.HideArrow ();
+		//	arrowManage.HideArrow ();
 			gameManager.gameOver();
 			this.enabled = false;
 		}
@@ -40,6 +44,7 @@ public class dancing : MonoBehaviour {
 		// Change direction every 5 seconds
 		changeDirectionTimer += Time.deltaTime;
 		if (changeDirectionTimer >= timeBeforeChangeDirection) {
+			arrowAnimator.SetBool("isError", false);
 			ChangeDirection();
 
 			// If move never made
@@ -61,10 +66,11 @@ public class dancing : MonoBehaviour {
 
 				if (directionPushed == currentDirection) { // If made the correct move
 					myAudio.Play ();
-					if (heartParticles.isPlaying) heartParticles.Stop();
+					//if (heartParticles.isPlaying) heartParticles.Stop();
 					if (!heartParticles.isPlaying) heartParticles.Play();
 				} else { // If made wrong move
 					// Turn arrow red then fade out
+					arrowAnimator.SetBool("isError", true);
 					playerDancingHealth--;
 				}
 
@@ -80,25 +86,29 @@ public class dancing : MonoBehaviour {
 			
 			animator.SetInteger("state", 3);
 			currentDirection = "left"; 
-			arrowManage.ShowArrow ("left");
+		//	arrowManage.ShowArrow ("left");
+			arrowAnimator.SetTrigger("arrowLeftPlay");
 
 		} else if (random == 1) { //RIGHT
 			
 			animator.SetInteger("state", 4);
 			currentDirection = "right"; 
-			arrowManage.ShowArrow ("right");
+		//	arrowManage.ShowArrow ("right");
+			arrowAnimator.SetTrigger("arrowRightPlay");
 
 		} else if (random == 2) { // UP
 			
 			animator.SetInteger("state", 1);
 			currentDirection = "up";
-			arrowManage.ShowArrow ("up");
+		//	arrowManage.ShowArrow ("up");
+			arrowAnimator.SetTrigger("arrowUpPlay");
 
 		} else if (random == 3) { // DOWN
 			
 			animator.SetInteger("state", 2);
 			currentDirection = "down";  
-			arrowManage.ShowArrow ("down");
+		//	arrowManage.ShowArrow ("down");
+			arrowAnimator.SetTrigger("arrowDownPlay");
 		}
 	}
 
